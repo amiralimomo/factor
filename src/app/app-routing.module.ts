5bdd9483fe757531bb304d6ec03cc88f465da79c
Amir Ali Mousa <amiralimomo77@gmail.com>
2021-06-22 23:00:56 +0430

    fix imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:"" ,loadChildren:()=>import('./factor/factor.module').then(m=>m.FactorModule) },
-  {path:"security" ,loadChildren:()=>import('./factor/modules/security/security.module').then(m=>m.SecurityModule) }
+  {path:"" ,loadChildren:()=>import('./factor/factor.module').then(m=>m.FactorModule) }
+
 ];
 
 @NgModule({
--- src/app/factor/factor-routing.module.ts
+++ src/app/factor/factor-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path:"",component:DashboardComponent}
+  {path:"",component:DashboardComponent},
+  {path:"security" ,loadChildren:()=>import('./modules/security/security.module').then(m=>m.SecurityModule) }
 ];
 
 @NgModule({
